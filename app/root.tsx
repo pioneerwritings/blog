849246cdd42dc891cc52f2e71045df628d6c31ba
Joel Rivera <pioneerwritings@gmail.com>
2023-03-28 20:57:14 -0500

    fix: remove edge configs

--- app/root.tsx
+++ app/root.tsx
@@ -163,5 +163,3 @@ export default function App() {
     </html>
   )
 }
-
-export const config = { runtime: 'edge' }
--- app/routes/tag.$id.tsx
+++ app/routes/tag.$id.tsx
@@ -107,5 +107,3 @@ export default function DynamicTagPage() {
     </>
   )
 }
-
-export const config = { runtime: 'edge' }
