57ade33d1abeee15dd3b7aa307b9a1498a5ad09a
Joel Rivera <pioneerwritings@gmail.com>
2023-03-30 19:53:51 -0500

    chore: merge queries in one file

--- app/db/queries/index.ts
+++ app/db/queries/index.ts
@@ -1,4 +1,90 @@
-export * from './getPostByID'
-export * from './getPosts'
-export * from './getTags'
-export * from './getPostsByTag'
+import { gql } from 'graphql-request'
+
+export const QueryGetOGImage = gql`
+  query GetOGImage($id: ID!) {
+    asset(where: { id: $id }) {
+      url
+      width
+      height
+    }
+  }
+`
+
+export const QueryGetTags = gql`
+  query GetTags {
+    tags {
+      id
+      text
+    }
+  }
+`
+
+export const QueryGetPostsByTag = gql`
+  query GetPostsByTag($tag: String!) {
+    posts(where: { tag: { text: $tag } }) {
+      id
+      slug
+      title
+      excerpt
+      publishedAt
+      tag {
+        text
+      }
+      author {
+        name
+        title
+        picture {
+          url
+        }
+      }
+    }
+  }
+`
+
+export const QueryGetPosts = gql`
+  query GetPosts {
+    posts {
+      id
+      slug
+      title
+      excerpt
+      publishedAt
+      tag {
+        text
+      }
+      author {
+        name
+        title
+        picture {
+          url
+        }
+      }
+    }
+  }
+`
+
+export const QueryGetPostBySlug = gql`
+  query GetPost($slug: String!) {
+    post(where: { slug: $slug }) {
+      title
+      publishedAt
+      content {
+        html
+      }
+      tag {
+        text
+      }
+      author {
+        name
+        title
+        picture {
+          url
+        }
+      }
+      seo {
+        title
+        description
+      }
+    }
+  }
+`
